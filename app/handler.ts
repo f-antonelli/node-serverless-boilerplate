3034bfdf78a15d0e1cdbfc7b60b59e80f60b1402
f-antonelli <antonelli.fd@gmail.com>
2024-01-12 10:56:03 -0300

    ADD: setup tipescrypt

---
+++ app/handler.ts
@@ -0,0 +1,16 @@
+import { APIGatewayProxyCallbackV2 } from "aws-lambda";
+
+export const Signup = async (event: APIGatewayProxyCallbackV2) => {
+  console.log(event);
+  
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({
+      message: "response from signup",
+      data: [],
+    }),
+  };
+};
